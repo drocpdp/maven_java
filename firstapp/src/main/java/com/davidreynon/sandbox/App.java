e14b19412b5b6392115a43623f743748d8a0c150
David Eynon <davidreynon@gmail.com>
2014-11-23 23:27:58 -0800

    initial changes

---
+++ firstapp/src/main/java/com/davidreynon/sandbox/App.java
@@ -0,0 +1,16 @@
+package com.davidreynon.sandbox;
+import java.util.Date;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Date d = new Date();
+        System.out.println(d);
+    }
+}
