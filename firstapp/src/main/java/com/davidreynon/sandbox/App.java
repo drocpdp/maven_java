1f1880bfa22f6e2528c76345ba254e109adfb3f6
David Eynon <davidreynon@gmail.com>
2014-11-29 23:55:46 -0800

    oh yeah thats how arrays worked, now i remember

--- firstapp/src/main/java/com/davidreynon/sandbox/App.java
+++ firstapp/src/main/java/com/davidreynon/sandbox/App.java
@@ -1,5 +1,11 @@
 package com.davidreynon.sandbox;
 import java.util.Date;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Hello world!
@@ -12,5 +18,15 @@ public class App
         System.out.println( "Hello World!" );
         Date d = new Date();
         System.out.println(d);
+        int[] array1;
+        array1 = new int[10];
+        for (int x=0; x < array1.length; x++){
+        	array1[x] = x;
+        }
+        for (int x=0; x < array1.length; x++){
+        	System.out.println(array1[x]);
+        }
+        
+        
     }
 }
